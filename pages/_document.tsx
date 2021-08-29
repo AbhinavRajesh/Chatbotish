3d619403500b73ede6c16b8eb733aa57229ff3f0
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2021-08-29 12:13:16 +0530

    Favicon update and added chatbotish to chatbotish

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,16 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/logo192.png"></link>
           <meta name="theme-color" content="#113300" />
+          <link
+            href="https://chatbotish.vercel.app/index.css"
+            rel="stylesheet"
+          />
+          <script
+            src="https://chatbotish.vercel.app/index.js"
+            data-chatbotish
+            data-id="Pa1XoprGcKt1UO7H328k"
+            async
+          ></script>
         </Head>
         <body>
           <Main />
